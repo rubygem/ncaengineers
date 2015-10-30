8dfdd89ca615c6cefaa87ae67eee32fe20ada49c
rubygem <gemma.cameron@gmail.com>
2015-10-30 22:37:28 +0000

    initialising

---
+++ Gemfile
@@ -0,0 +1,21 @@
+ruby '2.2.1'
+source 'https://rubygems.org'
+
+gem 'bson_ext'
+gem 'rake'
+gem 'puma'
+gem 'therubyracer'
+gem 'sass'
+gem 'sinatra', :git => 'https://github.com/sinatra/sinatra.git'
+gem 'httparty'
+gem 'sinatra-assetpack'
+gem 'omniauth-authic'
+gem 'activesupport'
+gem 'rack-ssl-enforcer'
+
+group :development do
+	gem 'git_repository'
+	gem 'rack-test'
+	gem 'test-unit'
+	gem 'rack'
+end
---
+++ site.rb
@@ -0,0 +1,41 @@
+require 'bundler/setup'
+require 'json'
+require 'sinatra'
+require 'sass'
+require 'erb'
+require 'sinatra/assetpack'
+require 'tilt/erb'
+
+class App < Sinatra::Application
+	def initialize(app=nil)
+		super(app)
+	end
+
+	register Sinatra::AssetPack
+	assets do
+		js :application, [
+			'js/*.js'
+		]
+
+		css :application, [
+			'css/*.css'
+		]
+
+		js_compression :jsmin
+		css_compression :sass
+	end
+
+	# 404 Error!
+	not_found do
+		status 404
+		erb :not_found, :layout => :layout
+	end
+
+	error do
+		erb :error, :layout => :layout
+	end
+
+	get '/' do
+		erb :index, :layout => :layout
+	end
+end
